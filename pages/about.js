008397c57c59ffd306d80e6246ed131030c4e2ad
Xunnamius (Markov) <git@xunnmarkov.local>
2018-12-30 16:57:48 -0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+/* @flow */
+
+import * as React from 'react'
+import Link from 'next/link'
+import Header from 'components/view/header'
+
+export default class About extends React.PureComponent<{||}> {
+    render() {
+        return (
+            <div>
+                <Header />
+                <p>This is the about page!</p>
+            </div>
+        );
+    }
+}
